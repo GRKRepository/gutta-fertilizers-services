a198d6476aa9fe2f5b15cf080dbb24f0a2064be0
RamaKoteswararao Gutta <gutta.ramakoteswararao@gmail.com>
2020-07-18 13:26:17 +0530

    Project creation
    new project created files

---
+++ guttafertilizers/src/main/java/com/gf/guttafertilizers/GuttafertilizersApplication.java
@@ -0,0 +1,13 @@
+package com.gf.guttafertilizers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuttafertilizersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuttafertilizersApplication.class, args);
+	}
+
+}
---
+++ guttafertilizers/src/test/java/com/gf/guttafertilizers/GuttafertilizersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gf.guttafertilizers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuttafertilizersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
